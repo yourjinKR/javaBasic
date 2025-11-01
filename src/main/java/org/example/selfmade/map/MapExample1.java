b909c4cc957aae5a8e5664d02a563d82fe021162
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-02 01:19:52 +0900

    feat(map) : map 테스트

---
+++ src/main/java/org/example/selfmade/map/MapExample1.java
@@ -0,0 +1,40 @@
+package org.example.selfmade.map;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapExample1 {
+    public static void main(String[] args) {
+        Map<Team, List<Member>> map = new HashMap<>();
+        Team team = new Team("홍철 없는 홍철 팀");
+        Member member1 = new Member("유재석");
+        Member member2 = new Member("정형돈");
+
+        map.put(team, List.of(member1, member2));
+    }
+}
+
+class Team {
+    private String name;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+class Member {
+    private String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
