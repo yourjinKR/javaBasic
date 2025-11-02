139aff32d3049b541e6df10475a37b4d802482a4
yourjin <155814455+yourjinKR@users.noreply.github.com>
2025-11-02 23:31:38 +0900

    feat(map) : map value에 List 넣기

---
+++ src/main/java/org/example/selfmade/map/MapExample2.java
@@ -0,0 +1,68 @@
+package org.example.selfmade.map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * value를 List로 사용하고 싶을때
+ */
+public class MapExample2 {
+    public static void main(String[] args) {
+
+        // 일반적 로직
+        List<Student> list = List.of(
+                new Student(1, "유어진"),
+                new Student(1, "유지민"),
+                new Student(3, "유재석")
+        );
+
+        Map<Integer, List<Student>> school = new HashMap<>();
+
+        List<Student> list1 = list.stream().filter(student -> student.getGrade() == 1).toList();
+        List<Student> list3 = list.stream().filter(student -> student.getGrade() == 3).toList();
+
+        school.put(1,list1);
+        school.put(2,list3);
+
+        System.out.println(school);
+
+
+        // computeIfAbsent를 활용한 만들기
+        Map<Integer, List<Student>> school2 = new HashMap<>();
+
+        list.forEach(student -> {
+            school2.computeIfAbsent(student.getGrade(), key -> new ArrayList<>()).add(student);
+        });
+
+        System.out.println(school2);
+    }
+}
+
+class Student {
+    int grade;
+    String name;
+
+    public Student(int grade, String name) {
+        this.grade = grade;
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "grade=" + grade +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
